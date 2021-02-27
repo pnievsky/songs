1027277036538946151676468c93107a351a279e
pnievsky <pniewski.b@o2.pl>
2021-02-27 10:11:24 +0100

    Calling Action Creators from Components Functional Components with Connect Conditional Rendering

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends React.Component {
   renderList() {
@@ -7,7 +8,10 @@ class SongList extends React.Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
               Select
             </button>
           </div>
@@ -19,6 +23,7 @@ class SongList extends React.Component {
   }
 
   render() {
+    console.log(this.props);
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
@@ -27,4 +32,4 @@ const mapStateToProps = (state) => {
   return { songs: state.songs };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
